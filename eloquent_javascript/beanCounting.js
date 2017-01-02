9116e400eebf948bbf47f51451b51d989f717b1e
George Eaton <george_eaton@hotmail.co.uk>
2017-01-02 17:04:36 +0000

    adds countChar function

--- eloquent_javascript/beanCounting.js
+++ eloquent_javascript/beanCounting.js
@@ -2,7 +2,17 @@ var countBs = function(string) {
   var count = 0;
   for (var i = 0; i <= string.length; i++) {
     if (string.charAt(i) === "B") {
-      count += 1
+      count += 1;
+    }
+  }
+  return count;
+}
+
+var countChars = function(string, character) {
+  var count = 0;
+  for (var i = 0; i <= string.length; i++) {
+    if (string.charAt(i) === character) {
+      count += 1;
     }
   }
   return count;
