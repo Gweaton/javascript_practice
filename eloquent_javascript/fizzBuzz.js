cabf5cda68b6a8cd3f49ff33723a09913033e7c3
George Eaton <george_eaton@hotmail.co.uk>
2017-01-02 12:07:19 +0000

    fizzbuzz completed

--- eloquent_javascript/fizzBuzz.js
+++ eloquent_javascript/fizzBuzz.js
@@ -1,6 +1,7 @@
-var output = "";
 for (var i = 1; i <= 100; i++) {
-  if (i % 3 === 0) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    output = "FizzBuzz"
+  } else if (i % 3 === 0) {
     output = "Fizz"
   } else if (i % 5 === 0) {
     output = "Buzz"
