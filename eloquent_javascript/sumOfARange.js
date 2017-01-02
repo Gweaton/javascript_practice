e6e0b1977d4d8fddfad5786ad94c52efee1d2701
George Eaton <george_eaton@hotmail.co.uk>
2017-01-02 17:18:03 +0000

    adds sum function

--- eloquent_javascript/sumOfARange.js
+++ eloquent_javascript/sumOfARange.js
@@ -1,7 +1,15 @@
 var range = function(start, end) {
-  var numbers = []
+  var numbers = [];
   for (var i = start; i <= end; i++) {
     numbers.push(i);
   }
   return(numbers);
+}
+
+var sum = function(array) {
+  var total = 0;
+  for (var i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return(total);
 }
