82f901605b7291ab5ef40b1d2cd8a48c76cf3478
George Eaton <george_eaton@hotmail.co.uk>
2017-01-02 12:15:12 +0000

    refactors fizzbuzz to remove one line

--- eloquent_javascript/fizzBuzz.js
+++ eloquent_javascript/fizzBuzz.js
@@ -1,10 +1,10 @@
 for (var i = 1; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    output = "FizzBuzz"
-  } else if (i % 3 === 0) {
-    output = "Fizz"
-  } else if (i % 5 === 0) {
-    output = "Buzz"
+  var output = "";
+  if (i % 3 === 0) {
+    output += "Fizz"
+  }
+  if (i % 5 === 0) {
+    output += "Buzz"
   } else {
     output = i
   };
