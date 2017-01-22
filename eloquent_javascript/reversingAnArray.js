b5b3caab49f4b9f2ff38d67f2720cdd59b518a4c
George Eaton <george_eaton@hotmail.co.uk>
2017-01-22 12:17:59 +0000

    refactors reversingAnArray

--- eloquent_javascript/reversingAnArray.js
+++ eloquent_javascript/reversingAnArray.js
@@ -1,15 +1,15 @@
-var reverseArray = function(array) {
-  var reversedArray = [];
-  for (i = array.length - 1; i >= 0 ; i--) {
-    reversedArray.push(array[i]);
+function reverseArray(array) {
+  var reversed = [];
+  for (i = 0; i < array.length; i++) {
+    reversed.unshift(array[i])
   }
-  return reversedArray;
+  return reversed;
 }
 
-var reverseInPlace = function(array) {
-  var reversedArray = [];
-  for (i = array.length - 1; i >= 0 ; i--) {
-    reversedArray.push(array[i]);
+function reverseArrayInPlace(array) {
+  for (i = 0; i < Math.floor(array.length / 2); i++) {
+    var element = array[i]
+    array[i] = array[array.length -1 -i]
+    array[array.length - 1 - i] = element
   }
-  array = reversedArray;
 }
