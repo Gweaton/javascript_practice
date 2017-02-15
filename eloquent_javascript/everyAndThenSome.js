11db4e7572b648e34de27e3bd9532e22c54982ca
George Eaton <george_eaton@hotmail.co.uk>
2017-02-15 09:03:07 +0000

    adds some function

--- eloquent_javascript/everyAndThenSome.js
+++ eloquent_javascript/everyAndThenSome.js
@@ -3,4 +3,11 @@ function every(array, matcher) {
     if (matcher(array[i]) === false) return false;
   }
   return true;
-}
+};
+
+function some(array, matcher) {
+  for (i = 0; i < array.length; i++) {
+    if (matcher(array[i]) === true) return true;
+  }
+  return false;
+};
