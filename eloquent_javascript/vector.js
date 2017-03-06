83738373f06579505432abaaef4091bc99775030
George Eaton <george_eaton@hotmail.co.uk>
2017-03-06 12:51:54 +0000

    adds length getter method to Vector

--- eloquent_javascript/vector.js
+++ eloquent_javascript/vector.js
@@ -10,3 +10,16 @@ Vector.prototype.plus = function(vector) {
 Vector.prototype.minus = function(vector) {
   return new Vector((this.x -= vector.x), (this.y -= vector.y))
 }
+
+function sumSquared(x, y) {
+  return (x*x) + (y*y)
+}
+
+function pythagoras(x, y) {
+  return Math.sqrt(sumSquared(x, y));
+}
+
+Object.defineProperty(Vector.prototype, 'length', {
+  get: function() { return pythagoras(this.x, this.y)
+  }
+});
